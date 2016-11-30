303d756b006f9c3e4c38b8dc9950539827416221
smartapant <smartapant@gmail.com>
2016-11-30 17:31:51 +0300

    feat(api): reddit api service

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import {RedditApiService} from '../../providers/reddit-api-service';
+
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [RedditApiService]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController, public redditApi: RedditApiService) {
+    redditApi.fetchHot().subscribe((data) => console.log(data))
   }
 
 }
--- src/providers/reddit-api-service.ts
+++ src/providers/reddit-api-service.ts
@@ -2,17 +2,41 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the RedditApiService provider.
+const BASE_URL: string =  'https://www.reddit.com/';
+const JSON_POSTFIX: string = '.json';
 
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class RedditApiService {
 
-  constructor(public http: Http) {
-    console.log('Hello RedditApiService Provider');
+  constructor(public http: Http) {}
+
+  public fetchHot() {
+    return this.http.get(BASE_URL + JSON_POSTFIX)
+      .map(this.redditCollectionToJson)
   }
 
+  public fetchNext(lastPostName: string) {
+    return this.http.get(BASE_URL + JSON_POSTFIX + '?count=' + 25 + '&after=' + lastPostName)
+      .map(this.redditCollectionToJson)
+  }
+
+  public fetchComments(post) {
+    let url: string = BASE_URL + post.permalink + JSON_POSTFIX;
+    return this.http.get(url)
+      .map(res => res.json()[1].data.children.map(c => c.data).filter(c => c.body))
+      .map(this.beautifyReplies)
+
+  }
+
+  public beautifyReplies(comments) {
+    return comments.map(comment => {
+      comment.replies = comment.replies ? comment.replies.data.children.map(reply => reply.data).filter(c => c.body) : [];
+      this.beautifyReplies(comment.replies);
+      return comment;
+    })
+  }
+
+  private redditCollectionToJson(response) {
+    return response.json().data.children.map(c => c.data)
+  }
 }
